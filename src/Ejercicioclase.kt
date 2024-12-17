408c4650aa6b78833681cfa0eacde7ff3665cfd9
Oscar-Code21 <oarriazag@iesrafaelalberti.es>
2024-12-17 15:22:03 +0100

    Ejercicio

---
+++ src/Ejercicioclase.kt
@@ -0,0 +1,25 @@
+// Tarea de clase
+
+class curso{
+    private var nombre: String = ""
+    private var codigo: String = ""
+
+    fun agregarAlumnos(){
+    }
+
+}
+class estudiante{
+    private var nombre: String = ""
+    private var dni: String = ""
+
+    fun incribirse(){
+    }
+    fun mostrainscritos(){
+    }
+}
+
+fun main(){
+    var curso = estudiante()
+    var estudiante = estudiante()
+
+}
